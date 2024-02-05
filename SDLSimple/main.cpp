11369393d3f72e44f9aaf852e41c18cf99a40bca
Selena Cheung <chselena20@gmai.com>
2024-02-05 14:06:44 -0500

    Initial Commit

---
+++ SDLSimple/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDLSimple
+//
+//  Created by Selena Cheung on 2/5/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
